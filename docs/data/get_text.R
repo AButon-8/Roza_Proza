dae7cf557d1477ed0849187dd2d62e0c4ba7092d
AButon-8 <newbuton@gmail.com>
2024-12-23 07:06:17 +0300

    csv update

--- docs/data/get_text.R
+++
@@ -1,9 +0,0 @@
-# Функция для скрапинга текста
-
-get_text <- function(url){
-  # Sys.sleep(1) # задержка между скрапингом страниц, если нужно, в скобках - время задержки
-  read_html(url, encoding = "Windows-1251") |> 
-    html_elements(".text") |> 
-    html_text2() |> 
-    paste(collapse=" ")
-}
